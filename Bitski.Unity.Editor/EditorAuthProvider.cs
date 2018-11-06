d7e1fd40cf5d756b5d4e031019a71f57adf1a69a
Patrick Tescher <patrick@outtherelabs.com>
2018-11-06 14:26:19 -0800

    New SDK Format

---
+++ Bitski.Unity.Editor/EditorAuthProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace Bitski.Unity.Editor
+{
+    using System;
+    using UnityEngine;
+
+    class EditorAuthProvider : MonoBehaviour, AuthProvider {
+        public EditorAuthProvider(String clientId) {
+            
+        }
+
+        public void SignIn(Action<User> callback)
+        {
+            throw new NotSupportedException("The Bitski sdk does not support signing in in the editor");
+        }
+
+
+        public void GetUser(Action<User> callback)
+        {
+            throw new NotSupportedException("The Bitski sdk does not support getting a user in the editor");
+        }
+    }
+}
---
+++ Bitski.Unity.WebGL/WebGLAuthProvider.cs
@@ -0,0 +1,70 @@
+﻿namespace Bitski.Unity.WebGL
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using UnityEngine;
+
+    class WebGLAuthProvider : MonoBehaviour, AuthProvider
+    {
+        [DllImport("__Internal")]
+        private static extern void BitskiWebGLInit(string clientId);
+
+        public WebGLAuthProvider(String clientId) {
+            BitskiWebGLInit(clientId);
+
+            var bridge = new GameObject("WebGLAuthProvider");
+            bridge.AddComponent<WebGLAuthProvider>();
+            bridge.transform.parent = gameObject.transform;
+
+            Debug.Log("Set up WebGLAuthProvider");
+
+            DontDestroyOnLoad(bridge);
+        }
+
+        [DllImport("__Internal")]
+        private static extern void BitskiWebGLSignIn();
+
+        private Action<User> SignInCallback;
+
+        public void SignIn(Action<User> callback) {
+            SignInCallback = callback;
+            BitskiWebGLSignIn();
+        }
+
+        public void DidSignIn(String jsonObject) {
+            if (jsonObject.Contains("error")) {
+                Debug.Log("Got an error signing in: " + jsonObject);
+            } else {
+                Debug.Log("Signed in as: " + jsonObject);
+                var user = JsonUtility.FromJson<User>(jsonObject);
+                SignInCallback(user);
+            }
+        }
+
+        [DllImport("__Internal")]
+        private static extern void BitskiWebGLGetUser();
+
+        private Action<User> GetUserCallback;
+
+        public void GetUser(Action<User> callback)
+        {
+            GetUserCallback = callback;
+            BitskiWebGLGetUser();
+        }
+
+        public void DidGetUser(String userJSON, String errorJSON)
+        {   
+            if (userJSON != null)
+            {
+                Debug.Log("Got a user: " + userJSON);
+                var user = JsonUtility.FromJson<User>(userJSON);
+                GetUserCallback(user);
+            }
+
+            if (userJSON != null)
+            {
+                Debug.Log("Got an error getting user: " + errorJSON);
+            }
+        }
+    }
+}
